a3f05c80d59888212ce4929ea2ba7429225985ad
NicoCataldo <101023955+NicoCataldo@users.noreply.github.com>
2024-05-13 22:25:49 -0300

    Back

---
+++ BackReact/src/main/java/BackReact/BackReact/BackReactApplication.java
@@ -0,0 +1,13 @@
+package BackReact.BackReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackReactApplication.class, args);
+	}
+
+}
---
+++ BackReact/src/test/java/BackReact/BackReact/BackReactApplicationTests.java
@@ -0,0 +1,13 @@
+package BackReact.BackReact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
