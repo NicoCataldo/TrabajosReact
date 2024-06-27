053fe4e3a5ef5a77779f195236ecf4100d424d5c
Nico Cataldo <nicocataldo18@gmail.com>
2024-06-27 15:55:16 -0300

    .

--- app-react/src/Componentes/Home/Home.tsx
+++ app-react/src/Componentes/Home/Home.tsx
@@ -12,7 +12,7 @@ export const Home = () => {
         </div>
         <div className="Descripcion">
           <h2>
-            Tienda Hendrix es una tienda de instrumentos musicales con ya más de
+            Tienda Angus es una tienda de instrumentos musicales con ya más de
             15 años de experiencia. Tenemos el conocimiento y la capacidad como
             para informarte acerca de las mejores elecciones para tu compra
             musical.
